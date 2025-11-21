1f7812a010cc961bd01c31b9519de9d3e51e3cce
Aniketsingh0302 <anikets32002@gmail.com>
2025-11-21 19:52:03 +0530

    Whole app of image classification

---
+++ app.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+from homer_bart_prediction import predict_image,preprocess_image
+import streamlit as st
+
+st.set_page_config(page_title="Homer vs Bart Classifier", page_icon="🟡")
+
+# Cache model for performance
+@st.cache_resource
+def load_my_model():
+    return load_model("homer_bart_model.keras")
+
+model = load_my_model()
+
+st.title(" Homer vs Bart Classifier")
+st.write("Upload an image and the model will predict whether it's **Homer** or **Bart**.")
+
+uploaded = st.file_uploader("Upload image", type=["jpg", "jpeg", "png", "bmp"])
+
+if uploaded:
+    # Show image
+    st.image(uploaded, caption="Uploaded Image", use_column_width=True)
+
+    # Read image as bytes
+    bytes_data = uploaded.read()
+
+    # Predict
+    label, prob = predict_image(model, bytes_data)
+
+    st.subheader(f"🧠 Prediction: **{label}**")
+
+   
+    st.write(f"Upload another image of your friend {lable} ")
+
+
+    st.write(f"Enjoyed the app? Share it with your friends! 🚀")
+
---
+++ homer_bart_prediction.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "homer_bart_model.keras")
+model = load_model(MODEL_PATH)
+
+IMG_SIZE =(128,128)
+def preprocess_image(bytes_data):
+    #imgs = tf.io.read_file(bytes_data)
+    img = tf.image.decode_image(bytes_data, channels=3)
+    img = tf.image.resize(img, IMG_SIZE)
+    img = img / 255.0  # Normalize to [0,1]
+    img = tf.expand_dims(img, axis=0)  # Add batch dimension cnn need batch 
+    return img    
+class_name={0:'Homer',1:'Bart' }
+def predict_image(model, path):
+    img =preprocess_image(path)
+    prob =model.predict(img)[0][0] #predict for first element that's why [0][0]
+    label =1 if prob >0.5 else 0
+    return class_name[label],float(prob)
+if __name__ == "__main__":
+    import sys
+    image_path = sys.argv[1]
+    label, probability = predict_image(image_path)
+    print(f"Predicted Label: {label}, Probability: {probability:.4f}")
